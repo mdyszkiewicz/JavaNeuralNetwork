39b8f2c8d5fcab1fe930a86252a337959202d93f
Mateusz Dyszkiewicz <mati.bajan@gmail.com>
2026-08-17 18:38:43 +0200

    Added extra tests

--- Jave Neural Network/j-neural-net-java/src/main/java/NeuralNetwork.java
+++ Jave Neural Network/j-neural-net-java/src/main/java/NeuralNetwork.java
@@ -7,14 +7,19 @@ import java.util.stream.IntStream;
 public class NeuralNetwork {
 
     private final Map<Integer, List<Double>> biases;
-    private final List<Integer> layerSizes;
 
     public NeuralNetwork(List<Integer> layerSizes) {
-        this.layerSizes = layerSizes;
+        vetoNotEnoughLayers(layerSizes);
         biases = IntStream.range(1, layerSizes.size()).boxed().collect(Collectors.toMap(
                 layerNumber -> layerNumber,
                 layerNumber -> IntStream.range(0, layerSizes.get(layerNumber))
                         .mapToObj(i -> Math.random()).collect(Collectors.toList())));
+    }
+
+    private void vetoNotEnoughLayers(List<Integer> layerSizes) {
+        if (layerSizes == null || layerSizes.size() < 2) {
+            throw new RuntimeException("Layer sizes must be greater than 1 (and non null)");
+        }
     }
 
     public Map<Integer, List<Double>> getBiases() {
--- Jave Neural Network/j-neural-net-java/src/test/java/NeuralNetworkTest.java
+++ Jave Neural Network/j-neural-net-java/src/test/java/NeuralNetworkTest.java
@@ -9,11 +9,7 @@ import static org.junit.Assert.assertNotEquals;
 
 public class NeuralNetworkTest {
 
-    @Test
-    public void getBiases() {
-        NeuralNetwork neuralNetwork = new NeuralNetwork(Arrays.asList(16, 4));
-
-
+    private static void assertBiasesAreMostlyNonZero(NeuralNetwork neuralNetwork) {
         long nonZeroBiases = 0;
         for (List<Double> biases : neuralNetwork.getBiases().values()){
             for (Double bias : biases) {
@@ -22,9 +18,26 @@ public class NeuralNetworkTest {
                 }
             }
         }
-
         assertEquals(1, neuralNetwork.getBiases().size());
         assertNotEquals(0, nonZeroBiases);
+    }
+
+    @Test
+    public void getBiases() {
+        NeuralNetwork neuralNetwork = new NeuralNetwork(Arrays.asList(16, 4));
+
+        assertEquals(4, neuralNetwork.getBiases().get(1).size());
+        assertBiasesAreMostlyNonZero(neuralNetwork);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getInvalidNetworkSize() {
+        new NeuralNetwork(Arrays.asList(16));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getNullNetworkSize() {
+        new NeuralNetwork(null);
     }
 
 }

8f2d01394d1de8671298454eb00902ec6048b44e
Mateusz Dyszkiewicz <mati.bajan@gmail.com>
2026-08-18 20:27:45 +0200

    Implemented calculating value of a neuron

--- Jave Neural Network/j-neural-net-java/src/main/java/mattd/Neuron.java
+++ Jave Neural Network/j-neural-net-java/src/main/java/mattd/Neuron.java
@@ -5,19 +5,25 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Neuron {
     private final List<Pair<Neuron, Double>> weightedInputs;
-    private Double bias;
+    private final Double bias;
+    private final TransformationFunction transformationFunction;
 
-    Neuron(List<Pair<Neuron, Double>> weightedInputs, Double bias) {
+    Neuron(List<Pair<Neuron, Double>> weightedInputs, Double bias, TransformationFunction transformationFunction) {
         this.weightedInputs = weightedInputs;
         this.bias = bias;
+        this.transformationFunction = transformationFunction; // no other implementations yet
     }
 
+
     public Double getValue() {
-        return 0.0;
+        return transformationFunction.apply(weightedInputs.stream()
+                .mapToDouble(weightedNeuron -> weightedNeuron.getRight() * weightedNeuron.getLeft().getValue())
+                .reduce(Double::sum).orElse(0.0) + getBias());
     }
 
     public Double getBias() {
@@ -30,12 +36,43 @@ public class Neuron {
 
     public static class NeuronBuilder {
 
+        public static final SigmoidFunction DEFAULT_TRANSFORMATION_FUNCTION = new SigmoidFunction();
+
         public static Neuron createNeuronWithInputs(List<Pair<Neuron, Double>> weightedInputs, Double bias) {
-            return new Neuron(weightedInputs, bias);
+            return createNeuronWithInputsUsingOutputTransformingFunction(weightedInputs, bias, DEFAULT_TRANSFORMATION_FUNCTION);
+        }
+
+        public static Neuron createNeuronWithInputsUsingOutputTransformingFunction(List<Pair<Neuron, Double>> weightedInputs, Double bias, TransformationFunction transformationFunction) {
+            return new Neuron(weightedInputs, bias, transformationFunction);
         }
 
         public static Neuron createNeuronWithValue(Double value) {
-            return new Neuron(Collections.emptyList(), value);
+            return createNeuronWithValueUsingOutputTransformingFunction(value, DEFAULT_TRANSFORMATION_FUNCTION);
+        }
+
+        public static Neuron createNeuronWithValueUsingOutputTransformingFunction(Double value, TransformationFunction defaultTransformationFunction) {
+            return createNeuronWithInputsUsingOutputTransformingFunction(Collections.emptyList(), value, defaultTransformationFunction);
+        }
+
+    }
+
+
+    public interface TransformationFunction extends Function<Double, Double> {
+
+    }
+
+    public static class SigmoidFunction implements TransformationFunction {
+        @Override
+        public Double apply(Double value) {
+            return 1 / (1 + Math.exp(-value));
+        }
+    }
+
+
+    public static class TransparentValue implements TransformationFunction {
+        @Override
+        public Double apply(Double value) {
+            return value;
         }
     }
 }
--- Jave Neural Network/j-neural-net-java/src/test/java/mattd/NeuronTest.java
+++ Jave Neural Network/j-neural-net-java/src/test/java/mattd/NeuronTest.java
@@ -26,7 +26,21 @@ class NeuronTest {
         assertTrue(weights.containsAll(Arrays.asList(0.4, 0.2)));
     }
 
+    @Test
+    void testValue() {
+        // given
+        List<Pair<Neuron, Double>> inputs = Arrays.asList(createInputNeuron(0.0, 0.4), createInputNeuron(1.0, 0.2));
+        Neuron neuron = Neuron.NeuronBuilder.createNeuronWithInputsUsingOutputTransformingFunction(inputs, 0.5, new Neuron.TransparentValue());
+
+        // when
+        Double value = neuron.getValue();
+
+        // then
+        assertEquals(0.7, value);
+    }
+
     private static @NonNull ImmutablePair<Neuron, Double> createInputNeuron(double inputValue, double weight) {
-        return new ImmutablePair<>(Neuron.NeuronBuilder.createNeuronWithValue(inputValue), weight);
+        return new ImmutablePair<>(Neuron.NeuronBuilder
+                .createNeuronWithValueUsingOutputTransformingFunction(inputValue, new Neuron.TransparentValue()), weight);
     }
 }
